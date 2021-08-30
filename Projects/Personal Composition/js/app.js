1fd1d78045f6be7f44ef208e2bdc34a3fd57b766
Bnewton019 <newtonba@iu.edu>
2021-08-30 17:29:07 -0400

    Fixed Color Issues

--- Projects/Personal Composition/js/app.js
+++ Projects/Personal Composition/js/app.js
@@ -6,14 +6,16 @@ function setup() {
 }
 
 function draw () {
-    background(204);
+    background(35);
+    fill(255);
     ellipse(350, y, x, x);
     ellipse(450, y, x, x);
     ellipse(550, y, x, x);
     ellipse(650, y, x, x);
     ellipse(750, y, x, x);
-    arc(mouseX, 300, 200, 200, 0.47, 5.65);
     fill(255, 204, 0);
+    arc(mouseX, 300, 200, 200, 0.47, 5.65);
+    fill('blue');
      rect(0, 125, 800, 25);
      rect(0, 450, 800, 25);
     
