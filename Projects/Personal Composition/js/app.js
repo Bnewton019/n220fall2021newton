ce736a65c680fa74a6577d35582460e9abe290f0
Bnewton019 <newtonba@iu.edu>
2021-08-30 17:23:35 -0400

    Added Variables

--- Projects/Personal Composition/js/app.js
+++ Projects/Personal Composition/js/app.js
@@ -1,23 +1,21 @@
+var x = 25;
+var y = 300;
+
 function setup() {
     createCanvas(800, 600)
 }
 
 function draw () {
-   
     background(204);
-
-   
-    rect(0, 125, 800, 25);
-    rect(0, 450, 800, 25);
-
-    ellipse(350, 300, 25, 25);
-    ellipse(450, 300, 25, 25);
-    ellipse(550, 300, 25, 25);
-    ellipse(650, 300, 25, 25);
-    ellipse(750, 300, 25, 25);
-   
-    fill(255, 204, 0);
+    ellipse(350, y, x, x);
+    ellipse(450, y, x, x);
+    ellipse(550, y, x, x);
+    ellipse(650, y, x, x);
+    ellipse(750, y, x, x);
     arc(mouseX, 300, 200, 200, 0.47, 5.65);
+    fill(255, 204, 0);
+     rect(0, 125, 800, 25);
+     rect(0, 450, 800, 25);
     
    
 }
