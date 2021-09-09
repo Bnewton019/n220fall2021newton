6e89b16f4b9de38bb5e521b49b91892bdf77785f
Bnewton019 <newtonba@iu.edu>
2021-09-09 16:37:58 -0400

    Added Color to Changing Circle

--- Projects/Changing Circle/js/app.js
+++ Projects/Changing Circle/js/app.js
@@ -6,9 +6,13 @@ function setup() {
 }
 
 function draw() {
+    background(134, 142, 144)
+    fill(20, 205, 238)
     circle(400, 300, diameter)
     diameter = diameter + 1;
     if(diameter > 200) {
         diameter = 1;
     }
+    
 }
+
