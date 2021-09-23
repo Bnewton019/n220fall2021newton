5744ad37868f808d16a513ffec59af2c659e3111
Bnewton019 <newtonba@iu.edu>
2021-09-23 02:19:45 -0400

    Finished Snake

--- Projects/Snake/js/app.js
+++ Projects/Snake/js/app.js
@@ -1,17 +1,28 @@
-let xPositions = [];
+let xPositions = [ 0,0,0,0,0,0,0,0,0,0];
+let yPositions = [ 0,0,0,0,0,0,0,0,0,0];
 
 function setup() {
     createCanvas(600,300);
-    Fill(255, 12, 129);
+    fill(255, 12, 129);
 }
 
 function draw() {
+    console.log(xPositions);
     background(0);
-    if(xPositions.length > 10) {
+    if(xPositions.length > 9) {
         xPositions.shift();
     }
+    if(yPositions.length > 9) {
+        yPositions.shift();
+    }
     xPositions.push(mouseX);
+    yPositions.push(mouseY);
+    
     for(var i = 0; i < xPositions.length; i++) {
-        circle(xPositions[i], 10, i*5);
+        for(var t = 0; t < yPositions.length; t++) {
+            circle(xPositions[i], yPositions[t], i*5);
+
+        }
+        
     }
 }
