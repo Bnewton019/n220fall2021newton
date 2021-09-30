c0e6c8de69d03305096a65f4debbbec345fbacc6
Bnewton019 <newtonba@iu.edu>
2021-09-30 15:45:26 -0400

    Peak Pixels Work

---
+++ Projects/Peak Pixels/js/app.js
@@ -0,0 +1,21 @@
+let dvSquare = document.getElementById("square");
+let x = "100px"
+
+dvSquare.style.backgroundColor = "#1f7a37";
+dvSquare.style.width = x;
+dvSquare.style.height = x;
+dvSquare.style.cursor = 'pointer';
+function sizeIncrease() {
+        let x = x*1.1
+        
+}
+
+
+
+function setup() {
+    createCanvas(800, 600);
+}
+
+
+
+
