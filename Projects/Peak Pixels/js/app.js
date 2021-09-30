6448d8e9667f48e085e269a9e01a9d49ac84c243
Bnewton019 <newtonba@iu.edu>
2021-09-30 16:47:11 -0400

    More work

--- Projects/Peak Pixels/js/app.js
+++ Projects/Peak Pixels/js/app.js
@@ -1,13 +1,15 @@
 let dvSquare = document.getElementById("square");
 let x = "100px"
 
+
 dvSquare.style.backgroundColor = "#1f7a37";
-dvSquare.style.width = x;
+dvSquare.style.width =  x;
 dvSquare.style.height = x;
 dvSquare.style.cursor = 'pointer';
-function sizeIncrease() {
-        let x = x*1.1
-        
+dvSquare.onclick = function() {
+    
+        x = x*0.1
+    
 }
 
 
